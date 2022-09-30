6d20ffbdf393eb6f1b053fdeb6b9d80199da4081
redolf <redolf639@gmail.com>
2022-09-30 21:24:51 +0000

    mongoosing

--- app.js
+++ app.js
@@ -1,41 +1,118 @@
 //require mongoose 
+const express = require('express');
+const { FindCursor } = require('mongodb');
 const mongoose = require('mongoose'); 
+const { stringify } = require('querystring');
 
-//connection 
+//open connection 
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-//structure or Schema of data that will be saved to DB
+//structure or fruit Schema of data that will be saved to DB
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Masa enter a name."], 
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
+//structure or person Schema of data that will be saved to DB
 const personSchema = new mongoose.Schema({
     name: String,
-    age: Number
-})
+    age: Number,
+    favouriteFuite: fruitSchema
+});
 
 //use schema to create a mongoose model;
 const Fruit = mongoose.model("Fruit", fruitSchema); 
 const Person = mongoose.model("Person", personSchema); 
 
-//create a new dec
-const fruit = new Fruit({
-    name: "Bananas",
-    rating: 8,
-    review: "Taste good, healthy"
-});  
+//create a new doc
+const mango = new Fruit({
+    name: "mango",
+    rating: 7,
+    review: "It's okay"
+});
+mango.save();
+
 
 const person = new Person({
-    name: "Oppong",
-    age: 37
+    name: "Nkrumah",
+    age: 37,
+    favouriteFuite: mango
 });
+person.save();
+
+
 
 //insert single doc of data in db; 
-fruit.save();
-person.save()
+//fruit.save();
 
+
+
+Fruit.find((err,fruits) => {
+    
+        if(err){
+            console.log(err);
+        } else{
+            //close the connection
+            mongoose.connection.close();
+            fruits.forEach((fruit) => {
+            console.log(fruit.name);
+            //when I kept the close connection down here, it worked but I got warnings; 
+            });
+        }
+});
+
+
+
+/*
+//Deleting many records; 
+Person.deleteMany({name:"Oppong"},(err)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Sucessfully Deleted!");
+    }
+});
+*/
+
+/*
+//Deleting a record;
+Fruit.deleteOne({name:"Carrots"},(err)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Successfully deleted!");
+    }
+});
+*/
+
+/*
+//Updating a record; 
+Fruit.updateOne({_id: "63371686f1f49bcbeba7458b"}, {name: "Carrots"},(err)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log("This has been successfully update");
+    }
+});
+*/
+
+
+
+
+
+
+
+
+
+/*
 const oranges = new Fruit({
     name: "Orange",
     rating: 7,
@@ -51,8 +128,10 @@ const carrots = new Fruit({
     rating: 7,
     review: "It's okay"
 });
+*/
 
 //inserting many in the db; 
+/*
 Fruit.insertMany([oranges,mango,carrots], (err) => {
     if(err) {
         console.log(err);
@@ -60,6 +139,4 @@ Fruit.insertMany([oranges,mango,carrots], (err) => {
         console.log("sucess!!!");
     }
 });
-
-
-
+*/
